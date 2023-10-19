f07156fde23dd80484fdf902a7f721df91ba5704
Danny CHUN JEN Yang <yjring@gmail.com>
2023-10-19 17:08:58 +0800

    ps1 modify

--- setup.ps1
+++ setup.ps1
@@ -1,28 +1,34 @@
-# 讀取 dockerAccount.txt 的內容
-$DockerAcc = Get-Content .\dockerAccount.txt
-# 讀取 cluster1-IP.txt 和 cluster2-IP.txt 的內容
-$newCluster1IP = Get-Content .\cluster1-IP.txt
-$newCluster2IP = Get-Content .\cluster2-IP.txt
-
 # 設定要被替換的原始字串
 $OrigStr = "yjring"
 
-# 進入到 ./github/workflows/ 
-Set-Location .\.github\workflows\
+# 讀取 env.yaml，並存入相應變數
+$envContent = Get-Content -Path env.yaml
+$DOCKER_ACCOUNT = ($envContent -match "dockerAccount:\s*([\S]+)") ? $matches[1] : $null
+$OS_ARCH = ($envContent -match "os:\s*([\S]+)") ? $matches[1] : $null
+
+switch ($OS_ARCH) {
+    "arm", "arm64" {
+        $PLATFORMS = "linux/arm64"
+    }
+    "amd", "amd64" {
+        $PLATFORMS = "linux/amd64"
+    }
+    default {
+        $PLATFORMS = "linux/amd64,linux/arm64"
+    }
+}
+
+# 進入到 .github/workflows/
+Set-Location -Path .\.github\workflows\
 
 # 列出要處理的檔案
 $files = "1_dev_backend_ci.yaml", "2_dev_frontend_ci.yaml", "4_uat_backend_ci.yaml", "5_uat_frontend_ci.yaml"
 
 # 更換Github Action中的 image檔案名稱
 foreach ($file in $files) {
-    # 使用 PowerShell 來替換 IMAGE_NAME: $OrigStr 為 IMAGE_NAME: $DockerAcc
-    (Get-Content $file) -replace "IMAGE_NAME: $OrigStr", "IMAGE_NAME: $DockerAcc" | Set-Content $file
-    # 印出已修改完成的檔案名
+    $fileContent = Get-Content -Path $file
+    $fileContent = $fileContent -replace "IMAGE_NAME: $OrigStr", "IMAGE_NAME: $DOCKER_ACCOUNT"
+    $fileContent = $fileContent -replace "platforms:.*", "platforms: $PLATFORMS"
+    Set-Content -Path $file -Value $fileContent
     Write-Output "Modified: $file"
 }
-
-Set-Location ..\..
-
-# yaml檔案的列表
-$dev_files = "deploy\dev\api-ingress.yaml", "deploy\dev\ui-ingress.yaml"
-$uat_files = "deploy\uat\api-ingress.yaml", "deploy\uat\ui-ingress.yaml"
